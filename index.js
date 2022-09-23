ef31f50ad027144d080893f7abf6ed41880cd68f
juanf.b <juanfbustamante94@gmail.com>
2022-09-23 17:04:45 -0300

    DiseñO y desarrollo general

---
+++ index.js
@@ -0,0 +1,44 @@
+
+// let headerIndex =document.getElementById("headerIndex")
+// let headerServices =document.getElementById("headerServices")
+// let headerAbout =document.getElementById("headerAbout")
+// let headerWork =document.getElementById("headerWork")
+// let mainIndex =document.getElementById("mainIndex")
+// let mainServices =document.getElementById("mainServices")
+// let mainAbout =document.getElementById("mainAbout")
+// let mainWork =document.getElementById("mainWork")
+
+// function tops() {
+//     headerIndex.scrollIntoView();
+//     headerServices.scrollIntoView();
+//     headerAbout.scrollIntoView();
+//     headerWork.scrollIntoView();
+//     window.setTimeout(function () {
+//         bottom();
+//     }, 8000);
+// }
+
+// function bottom() {
+//     mainIndex.scrollIntoView();
+//     mainServices.scrollIntoView();
+//     mainAbout.scrollIntoView();
+//     mainWork.scrollIntoView();
+// }
+
+// tops();
+
+const nav = document.querySelector('nav');
+
+window.addEventListener('scroll', function () {
+    const offset = window.pageYOffset;
+
+    if (offset > 75)
+        nav.classList.add('scroll')
+    // else
+    //     nav.classList.remove('scroll')
+});
+
+const preloaderWrapper = document.querySelector(".preloader-wrapper")
+window.addEventListener('load', function () {
+    preloaderWrapper.classList.add('fade-out-animation')
+})
