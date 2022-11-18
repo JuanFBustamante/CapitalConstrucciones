5b261976189ade660987eb5add565c1f26641ce4
juanf.b <juanfbustamante94@gmail.com>
2022-11-18 14:32:15 -0300

    form

--- index.js
+++ index.js
@@ -43,3 +43,40 @@ window.addEventListener('load', function () {
     preloaderWrapper.classList.add('fade-out-animation')
 })
 
+const $form = document.querySelector("#form")
+
+$form.addEventListener("submit", handleSubmit)
+
+async function handleSubmit(event) {
+    event.preventDefault()
+    const form = new FormData(this)
+
+    const response = await fetch(this.action, {
+        method: this.method,
+        body: form,
+        headers: {
+            "Accept": "application/json"
+        }
+    })
+
+
+
+    if (response.ok) {
+        $form.reset()
+        Swal.fire({
+            icon: 'success',
+            title: 'Mensaje enviado exitosamente',
+            text: 'Lo contactaremos en la brevedad',
+            confirmButtonColor: '#db7304',
+            confirmButtonText: 'Cerrar'
+        })
+    } else {
+        Swal.fire({
+            icon: 'error',
+            title: 'Error al enviar mensaje',
+            text: 'Verifique que los datos sean correctos',
+            confirmButtonColor: '#db7304',
+            confirmButtonText: 'Cerrar'
+        })
+    }
+}
