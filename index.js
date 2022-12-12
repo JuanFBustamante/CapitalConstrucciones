c480f9999face71c73d42689e3c6736eb722d4b9
juanf.b <juanfbustamante94@gmail.com>
2022-12-12 09:16:37 -0800

    textos

--- index.js
+++ index.js
@@ -1,32 +1,4 @@
 
-// let headerIndex =document.getElementById("headerIndex")
-// let headerServices =document.getElementById("headerServices")
-// let headerAbout =document.getElementById("headerAbout")
-// let headerWork =document.getElementById("headerWork")
-// let mainIndex =document.getElementById("mainIndex")
-// let mainServices =document.getElementById("mainServices")
-// let mainAbout =document.getElementById("mainAbout")
-// let mainWork =document.getElementById("mainWork")
-
-// function tops() {
-//     headerIndex.scrollIntoView();
-//     headerServices.scrollIntoView();
-//     headerAbout.scrollIntoView();
-//     headerWork.scrollIntoView();
-//     window.setTimeout(function () {
-//         bottom();
-//     }, 8000);
-// }
-
-// function bottom() {
-//     mainIndex.scrollIntoView();
-//     mainServices.scrollIntoView();
-//     mainAbout.scrollIntoView();
-//     mainWork.scrollIntoView();
-// }
-
-// tops();
-
 const nav = document.querySelector('nav');
 
 window.addEventListener('scroll', function () {
